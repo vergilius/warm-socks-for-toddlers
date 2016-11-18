5d5371f70098444a1cbd316dd2b5f93a05a051bc
dniziolek <dominika.niziolek@gmail.com>
2016-11-18 13:12:51 +0100

    simple protocole implementation

--- server/index.js
+++ server/index.js
@@ -7,17 +7,66 @@ const port = 3000;
 
 app.use(express.static(path.resolve(__dirname, '../client')));
 
-io.on('connection', (socket) => {
-  console.log('socket connected');
+let loggedInUsers = [];
 
-  socket.on('event', (data) => {
+function onAuthorize(socket, payload) {
+  loggedInUsers.push(payload);
+  myId = payload.id;
+
+  socket.emit('moj-kanal-komunikacji', { action: 'authorized' });
+}
+
+function getUsersInChannel(channel) {
+  return loggedInUsers.filter(u => u.channel === channel);
+}
+
+function onGetLoggedInUsers(socket,oldChannel,userId) {
+  let currentUser = getCurrentUser(userId);
+  
+  if (!currentUser) {
+    return;
+  }
+
+  let payload = getUsersInChannel(currentUser.channel);
+
+  socket.emit('moj-kanal-komunikacji', { action: 'logged-in-users', payload: payload });
+  
+  if(oldChannel) {
+    socket.broadcast.to(oldChannel).emit('moj-kanal-komunikacji', { action: 'user-has-left', payload: getUsersInChannel(currentUser.channel) });
+  }
+
+  socket.broadcast.to(currentUser.channel).emit('moj-kanal-komunikacji', { action: 'logged-in-users', payload: payload })
+}
+
+io.on('connection', (socket) => {
+
+  socket.on('moj-kanal-komunikacji', (data) => {
     console.log('event', data);
 
-    setTimeout(() => socket.emit('event', 'hey ho'), 1000);
+    switch (data.action) {
+      case 'authorize':
+        onAuthorize(socket, data.payload);
+        break;
+      case 'switch-channel':
+        socket.join(data.payload);
+        let currentUser = getCurrentUser(data.userId);
+        let oldChannel = currentUser.channel;
+        if (currentUser) {
+          currentUser.channel = data.payload;
+        }
+        onGetLoggedInUsers(socket, oldChannel, data.userId);
+        break;
+    }
   });
+
   socket.on('disconnect', () => {
-    console.log('disconnected')
+    console.log('user disconnect');
   });
 });
+
+
+function getCurrentUser(id) {
+  return loggedInUsers.filter(u => u.id === id)[0];
+}
 
 server.listen(port, () => console.log('server running at port:', port));
